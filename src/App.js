6bcc7a39377b48b16ecc56e933a6a391541e9771
Ronak <gondaliya.ronak100@gmail.com>
2025-12-04 12:07:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import quotes from "./Components/Quotedata"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [quote,setQuote] = useState("")
+
+  const generatequote=()=>{
+    const randomIndex=Math.floor(Math.random()*quotes.length)
+    setQuote(quotes[randomIndex])
+  }
+
+  return(<>
+    <div className="container">
+      <h1 className="title">Generate Quote</h1>
+      <p className="quote-box">{quote}</p>
+      <button className="btn" onClick={generatequote}>Generate Quote</button>
     </div>
-  );
+  </>
+  )
 }
-
-export default App;
