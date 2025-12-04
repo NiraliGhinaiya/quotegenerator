e585cfb1eddbdf699ebfa5b89754c0e1aabd2b29
Ronak <gondaliya.ronak100@gmail.com>
2025-12-04 12:10:55 +0530

    third commit

---
+++ src/Components/Quotedata.js
@@ -0,0 +1,13 @@
+const quotes=[
+    "Believe in yourself.",
+    "You are stronger than you think.",
+    "Never give up.",
+    "Stay positive, work hard.",
+    "The best way to predict the future is to create it.",
+    "The only way to do great work is to love what you do.",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    "The future belongs to those who believe in the beauty of their dreams.",
+    "The only way to do great work is to love what you do.",
+]
+
+export default quotes
